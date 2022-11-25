25c0df44a74854328e127b45a84a30a33245acef
ic__e <yoshidashi058@gmail.com>
2022-11-25 23:58:46 +0900

    feat: add react hook form

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,20 +1,45 @@
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+  confirmationPassword: string;
+};
 
 const signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
   return (
     <>
       <form action="">
         <div>
           <label>Email</label>
-          <input type="email" placeholder="example@hoge.com" />
+          <input
+            type="email"
+            placeholder="example@hoge.com"
+            {...register("email", { required: true })}
+          />
         </div>
         <div>
           <label>Password</label>
-          <input type="password" />
+          <input
+            type="password"
+            {...register("password", { required: true })}
+          />
+          {errors.password && <p>Please enter your password.</p>}
         </div>
         <div>
           <label>Confirm password</label>
-          <input type="password" />
+          <input
+            type="password"
+            {...register("confirmationPassword", { required: true })}
+          />
+          {errors.confirmationPassword && <p>Please re-enter your password.</p>}
         </div>
         <button type="submit">サインアップ</button>
       </form>
